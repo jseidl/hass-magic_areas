a43aed1f5cba7b1feefedb8c86ebb07f4e6f615f
Jan Seidl <janseidl@gmail.com>
2020-10-19 22:50:22 -0700

    moved all constants to const.py, using area.config.get() directly

--- custom_components/magic_areas/sensor.py
+++ custom_components/magic_areas/sensor.py
@@ -10,35 +10,23 @@ from homeassistant.components.light import DOMAIN as LIGHT_DOMAIN
 from homeassistant.components.media_player import DOMAIN as MEDIA_PLAYER_DOMAIN
 from homeassistant.components.sensor import DOMAIN as SENSOR_DOMAIN
 from homeassistant.components.switch import DOMAIN as SWITCH_DOMAIN
-from homeassistant.const import (
-    DEVICE_CLASS_CURRENT,
-    DEVICE_CLASS_ENERGY,
-    DEVICE_CLASS_HUMIDITY,
-    DEVICE_CLASS_ILLUMINANCE,
-    DEVICE_CLASS_POWER,
-    DEVICE_CLASS_TEMPERATURE,
-    STATE_ON,
-)
+from homeassistant.const import STATE_ON
 from homeassistant.helpers.entity import Entity
 from homeassistant.helpers.event import (
     async_track_state_change,
     async_track_time_interval,
 )
 
-from . import MODULE_DATA
+from .const import (
+    AGGREGATE_SENSOR_CLASSES,
+    CONF_EXTERIOR,
+    CONF_UPDATE_INTERVAL,
+    MODULE_DATA,
+)
 
 _LOGGER = logging.getLogger(__name__)
 
 SLEEP_TIME = 0.3  # seconds
-
-AGGREGATE_SENSOR_CLASSES = (
-    DEVICE_CLASS_CURRENT,
-    DEVICE_CLASS_ENERGY,
-    DEVICE_CLASS_HUMIDITY,
-    DEVICE_CLASS_ILLUMINANCE,
-    DEVICE_CLASS_POWER,
-    DEVICE_CLASS_TEMPERATURE,
-)
 
 
 async def async_setup_platform(
@@ -69,7 +57,7 @@ async def async_setup_platform(
             if device_class not in device_class_area_map.keys():
                 device_class_area_map[device_class] = {"exterior": [], "interior": []}
 
-            area_location = "exterior" if area.exterior else "interior"
+            area_location = "exterior" if area.config.get(CONF_EXTERIOR) else "interior"
             device_class_area_map[device_class][area_location].append(area)
 
             _LOGGER.info(f"Creating aggregate sensor for {area.name}/{device_class}")
@@ -126,7 +114,7 @@ class AreaSensorGroupSensor(Entity):
 
         # Track presence sensors
         async_track_state_change(hass, self.sensors, self.sensor_state_change)
-        delta = timedelta(seconds=self.area.update_interval)
+        delta = timedelta(seconds=self.area.config.get(CONF_UPDATE_INTERVAL))
 
         # Timed self update
         async_track_time_interval(self.hass, self.update_group, delta)
@@ -222,8 +210,8 @@ class GlobalSensorGroupSensor(Entity):
                 self.sensors.append(entity.entity_id)
                 uom_candidates.append(entity.unit_of_measurement)
 
-                if area.update_interval > self.update_interval:
-                    self.update_interval = area.update_interval
+                if area.config.get(CONF_UPDATE_INTERVAL) > self.update_interval:
+                    self.update_interval = area.config.get(CONF_UPDATE_INTERVAL)
 
         # Elect unit_of_measurement
         self._unit_of_measurement = max(set(uom_candidates), key=uom_candidates.count)
