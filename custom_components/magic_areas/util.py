1ba4a5785d9a622aee473002a90db8acf5a8453d
Jan Seidl <janseidl@gmail.com>
2022-07-02 18:01:13 -0700

    fixing regression (#224)

--- custom_components/magic_areas/util.py
+++ custom_components/magic_areas/util.py
@@ -1,12 +1,16 @@
 from collections import Iterable
 
+basestring = (str, bytes)
+
+
+def is_entity_list(item):
+    return isinstance(item, Iterable) and not isinstance(item, basestring)
+
 
 def flatten_entity_list(input_list):
 
-    basestring = (str, bytes)
-
     for i in input_list:
-        if isinstance(i, Iterable) and not isinstance(i, basestring):
+        if is_entity_list(i):
             for sublist in flatten_entity_list(i):
                 yield sublist
         else:
