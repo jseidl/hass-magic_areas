d665c117631e97516b5dea5bf0edc5dd71ab4d94
Jan Seidl <janseidl@gmail.com>
2023-01-08 14:01:37 -0800

    adding SUN_DOMAIN (sun.sun) to the config flow entity filter

--- custom_components/magic_areas/const.py
+++ custom_components/magic_areas/const.py
@@ -11,6 +11,7 @@ from homeassistant.components.media_player import DOMAIN as MEDIA_PLAYER_DOMAIN
 from homeassistant.components.remote import DOMAIN as REMOTE_DOMAIN
 from homeassistant.components.sensor import DOMAIN as SENSOR_DOMAIN
 from homeassistant.components.sensor import SensorDeviceClass
+from homeassistant.components.sun import DOMAIN as SUN_DOMAIN
 from homeassistant.components.switch import DOMAIN as SWITCH_DOMAIN
 from homeassistant.const import (
     STATE_ALARM_TRIGGERED,
@@ -555,4 +556,5 @@ CONFIG_FLOW_ENTITY_FILTER_EXT = CONFIG_FLOW_ENTITY_FILTER + [
     LIGHT_DOMAIN,
     MEDIA_PLAYER_DOMAIN,
     CLIMATE_DOMAIN,
+    SUN_DOMAIN,
 ]
