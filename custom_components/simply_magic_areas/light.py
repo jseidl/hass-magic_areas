627bab11f9f701c339bedbf16d84890b76ab8293
pinkfish <pinkfish@users.noreply.github.com>
2024-05-22 14:44:01 -0700

    Update light.py

--- custom_components/simply_magic_areas/light.py
+++ custom_components/simply_magic_areas/light.py
@@ -169,7 +169,7 @@ class AreaLightGroup(MagicLightGroup):
         )
 
     def _load_illuminance_sensors(self) -> None:
-        if SensorDeviceClass.ILLUMINANCE not in self.area.enties:
+        if SensorDeviceClass.ILLUMINANCE not in self.area.entities:
             return
         for component, entities in self.area.entities[SensorDeviceClass.ILLUMINANCE]:
             for entity in entities:
