ab2e3ca1f0236607eaf38b3498be2d11338701ea
Jan Seidl <janseidl@gmail.com>
2025-02-25 23:21:54 +0000

    basic fan group functionality

---
+++ tests/test_fan.py
@@ -0,0 +1,88 @@
+"""Tests for the BLE Tracker feature."""
+
+from collections.abc import AsyncGenerator
+from typing import Any
+
+import pytest
+from pytest_homeassistant_custom_component.common import MockConfigEntry
+
+from homeassistant.components.fan import DOMAIN as FAN_DOMAIN
+from homeassistant.const import STATE_OFF, STATE_ON, ATTR_ENTITY_ID
+from homeassistant.core import HomeAssistant
+
+from custom_components.magic_areas.const import (
+    CONF_ENABLED_FEATURES,
+    CONF_FEATURE_FAN_GROUPS,
+    DOMAIN,
+)
+
+from tests.common import (
+    assert_in_attribute,
+    assert_state,
+    get_basic_config_entry_data,
+    init_integration,
+    setup_mock_entities,
+    shutdown_integration,
+)
+from tests.const import DEFAULT_MOCK_AREA
+from tests.mocks import MockFan
+
+# Fixtures
+
+
+@pytest.fixture(name="fan_groups_config_entry")
+def mock_config_entry_ble_tracker() -> MockConfigEntry:
+    """Fixture for mock configuration entry."""
+    data = get_basic_config_entry_data(DEFAULT_MOCK_AREA)
+    data.update({CONF_ENABLED_FEATURES: {CONF_FEATURE_FAN_GROUPS: {}}})
+    return MockConfigEntry(domain=DOMAIN, data=data)
+
+
+@pytest.fixture(name="_setup_integration_fan_groups")
+async def setup_integration_ble_tracker(
+    hass: HomeAssistant,
+    fan_groups_config_entry: MockConfigEntry,
+) -> AsyncGenerator[Any]:
+    """Set up integration with BLE tracker config."""
+
+    await init_integration(hass, [fan_groups_config_entry])
+    yield
+    await shutdown_integration(hass, [fan_groups_config_entry])
+
+
+# Entities
+
+
+@pytest.fixture(name="entities_fan_multiple")
+async def setup_entities_fan_multiple(
+    hass: HomeAssistant,
+) -> list[MockFan]:
+    """Create multiple mock fans setup the system with it."""
+    mock_fan_entities: list[MockFan] = []
+    nr_fans: int = 3
+    for i in range(nr_fans):
+        mock_fan_entities.append(
+            MockFan(name=f"mock_fan_{i}", unique_id=f"unique_fan_{i}")
+        )
+    await setup_mock_entities(hass, FAN_DOMAIN, {DEFAULT_MOCK_AREA: mock_fan_entities})
+    return mock_fan_entities
+
+
+# Tests
+
+
+async def test_fan_group(
+    hass: HomeAssistant,
+    entities_fan_multiple: list[MockFan],
+    _setup_integration_fan_groups,
+) -> None:
+    """Test Fan groups functionality."""
+
+    fan_group_entity_id = (
+        f"{FAN_DOMAIN}.magic_areas_fan_groups_{DEFAULT_MOCK_AREA}_fan_group_fan"
+    )
+
+    fan_group_state = hass.states.get(fan_group_entity_id)
+    assert_state(fan_group_state, STATE_OFF)
+    for fan_entity in entities_fan_multiple:
+        assert_in_attribute(fan_group_state, ATTR_ENTITY_ID, fan_entity.entity_id)
